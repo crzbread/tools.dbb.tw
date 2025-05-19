feb55bc990f276fe410da934a2e87e8c32e4957d
DBB <qeroer@gmail.com>
2025-05-19 17:04:10 +0800

    feat: 完成所有菜單營養資料結構整理

--- src/app/subway/page.tsx
+++ src/app/subway/page.tsx
@@ -1,5 +1,22 @@
 "use client";
 
+import { allMenuItems } from "../data/subwayNutrition";
+import { MenuItem } from "@/app/types/menu";
+
 export default function Subway() {
+  // console.log(allMenuItems.filter((item) => item.Type === "Sandwiches"));
+  const types: string[] = [...new Set(allMenuItems.map((item) => item.Type))];
+
+  const groupedByType: Record<string, MenuItem[]> = allMenuItems.reduce(
+    (acc, item) => {
+      if (!acc[item.Type]) acc[item.Type] = [];
+      acc[item.Type].push(item);
+      return acc;
+    },
+    {} as Record<string, MenuItem[]>,
+  );
+
+  console.log(types);
+  console.log(groupedByType);
   return <div>123</div>;
 }
